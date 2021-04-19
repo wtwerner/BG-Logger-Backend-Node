e854ea1b0e833f3f0004c4be237c09cdd7e0a67f
wtwerner <wtwerner@gmail.com>
2021-04-19 17:28:22 -0500

    Revert to previous cors config

--- src/app.js
+++ src/app.js
@@ -7,24 +7,8 @@ const gameRouter = require('./routers/game')
 
 const app = express()
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested, Content-Type, Accept Authorization"
-    )
-    if (req.method === "OPTIONS") {
-      res.header(
-        "Access-Control-Allow-Methods",
-        "POST, PUT, PATCH, GET, DELETE"
-      )
-      return res.status(200).json({})
-    }
-    next()
-})
-
 app.use(express.json())
-// app.use(cors({credentials: true, origin: 'https://werner-bg-logger-client.herokuapp.com'}))
+app.use(cors({credentials: true, origin: 'https://werner-bg-logger-client.herokuapp.com'}))
 app.use(cookieParser())
 app.use(userRouter)
 app.use(gameRouter)
