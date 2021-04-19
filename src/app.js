d67d2175e285a07dabbd9c389218a78885a4b0ea
wtwerner <wtwerner@gmail.com>
2021-04-19 17:12:38 -0500

    Add allowCrossDomain function

--- src/app.js
+++ src/app.js
@@ -7,6 +7,20 @@ const gameRouter = require('./routers/game')
 
 const app = express()
 
+const allowCrossDomain = (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+}
+
 app.use(express.json())
 app.use(allowCrossDomain)
 app.use(cors({credentials: true, origin: 'https://werner-bg-logger-client.herokuapp.com'}))
