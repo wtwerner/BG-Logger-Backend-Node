b2fd07077239b76d7d5e5a67c793586efeb247a2
wtwerner <wtwerner@gmail.com>
2021-04-20 11:49:39 -0500

    Adjust auth middleware to work with auth0

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,22 +1,45 @@
-const jwt = require('jsonwebtoken')
-const User = require('../models/user')
+// const jwt = require('jsonwebtoken')
+// const User = require('../models/user')
 
-const auth = async (req, res, next) => {
-    try {
-        const token = req.cookies.token
-        const decoded = jwt.verify(token, process.env.JWT_SECRET)
-        const user = await User.findOne({ _id: decoded._id, 'tokens.token': token })
+// const auth = async (req, res, next) => {
+//     try {
+//         const token = req.cookies.token
+//         const decoded = jwt.verify(token, process.env.JWT_SECRET)
+//         const user = await User.findOne({ _id: decoded._id, 'tokens.token': token })
 
-        if (!user) {
-            throw new Error()
-        }
+//         if (!user) {
+//             throw new Error()
+//         }
 
-        req.token = token
-        req.user = user
-        next()
-    } catch (e) {
-        res.status(401).send({ error: 'Please authenticate' })
-    }
-}
+//         req.token = token
+//         req.user = user
+//         next()
+//     } catch (e) {
+//         res.status(401).send({ error: 'Please authenticate' })
+//     }
+// }
 
-module.exports = auth
+// module.exports = auth
+
+const jwt = require('express-jwt');
+const jwtAuthz = require('express-jwt-authz');
+const jwksRsa = require('jwks-rsa');
+
+const auth = jwt({
+  // Dynamically provide a signing key
+  // based on the kid in the header and 
+  // the signing keys provided by the JWKS endpoint.
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://wtwerner.us.auth0.com/.well-known/jwks.json`
+  }),
+
+  // Validate the audience and the issuer.
+  audience: 'https://werner-bg-logger-api.herokuapp.com/',
+  issuer: [`https://wtwerner.us.auth0.com/`],
+  algorithms: ['RS256']
+});
+
+module.exports = { auth }
